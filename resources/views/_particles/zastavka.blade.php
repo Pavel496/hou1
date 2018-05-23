6915894f11d1a4542e05bff86614adcaad313212
Pavel496 <butusovpn@mail.ru>
2018-05-23 13:29:25 +0300

    zastav1. Есть

--- resources/views/_particles/zastavka.blade.php
+++ resources/views/_particles/zastavka.blade.php
@@ -1,6 +1,20 @@
-<div id="trailer" class="is_overlay">
-  <video id="video" width="50%" height="auto" autoplay="autoplay" loop="loop" preload="auto">
-    <source src="/site_assets/img/home.mp4"></source>
-    {{-- <source src="book.webm" type="video/webm"></source> --}}
+{{-- <div id="trailer" class="is_overlay">
+  <video id="video" width="100%" height="auto" autoplay="autoplay" loop="loop" preload="auto">
+    <source src=></source>
+
   </video>
+</div> --}}
+    {{-- <source src="book.webm" type="video/webm"></source> --}}
+<div class="row">
+  <div class="col-md-6">
+    <video width="768" height=auto autoplay loop preload="auto">
+      <source src="/site_assets/img/house.mp4" type="video/mp4">
+      {{-- <source src="movie.ogg" type="video/ogg">
+    Your browser does not support the video tag. --}}
+    </video>
+  </div>
+  <div class="col-md-6">
+  <img  width="768" height=auto src="/site_assets/img/zastav.jpg" alt="Mountain View">
+</div>
+
 </div>
