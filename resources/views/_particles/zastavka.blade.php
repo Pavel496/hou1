8ca8132d9ba017042ffdb89ea72d7b3870adc64d
Pavel496 <butusovpn@mail.ru>
2018-05-23 14:25:15 +0300

    Заставка слайдер. Есть

--- resources/views/_particles/zastavka.blade.php
+++ resources/views/_particles/zastavka.blade.php
@@ -5,16 +5,34 @@
   </video>
 </div> --}}
     {{-- <source src="book.webm" type="video/webm"></source> --}}
+
+
 <div class="row">
   <div class="col-md-6">
     <video width="768" height=auto autoplay loop preload="auto">
       <source src="/site_assets/img/house.mp4" type="video/mp4">
-      {{-- <source src="movie.ogg" type="video/ogg">
-    Your browser does not support the video tag. --}}
     </video>
   </div>
   <div class="col-md-6">
-  <img  width="768" height=auto src="/site_assets/img/zastav.jpg" alt="Mountain View">
-</div>
+    <img id="image" src="/site_assets/img/zastav2.jpg" style="width:768px">
+  </div>
 
 </div>
+
+{{-- <img id="image" src="/site_assets/img/zastav2.jpg">
+<p></p> --}}
+
+<script>
+
+var imageSources = ["/site_assets/img/zastav1.jpg", "/site_assets/img/zastav2.jpg", "/site_assets/img/zastav3.jpg"]
+
+var index = 0;
+setInterval (function(){
+  if (index === imageSources.length) {
+    index = 0;
+  }
+  document.getElementById("image").src = imageSources[index];
+  index++;
+} , 5000);
+
+</script>
