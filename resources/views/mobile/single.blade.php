4f33aad6fe3a5ce91b38b5e679f76f78745a6cc1
Pavel496 <butusovpn@mail.ru>
2018-06-26 14:45:29 +0300

    SEO. Есть

--- resources/views/mobile/single.blade.php
+++ resources/views/mobile/single.blade.php
@@ -2,13 +2,19 @@
 <html lang="zxx">
 <head>
 	<meta charset="UTF-8">
-	<title>Country House Realty</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="mobile-web-app-capable" content="yes">
-	<meta name="apple-mobile-web-app-capable" content="yes">
-	<meta name="apple-touch-fullscreen" content="yes">
-	<meta name="HandheldFriendly" content="True">
+  <title>@yield('head_title', getcong('site_name'))</title>
+  <meta name="description" content="@yield('head_description', getcong('site_description'))">
+    <meta property="keywords" content="@yield('head_keywords', getcong('site_keywords'))" />
 
+    <meta property="og:type" content="article"/>
+    <meta property="og:title" content="@yield('head_title',  getcong('site_name'))"/>
+    <meta property="og:description" content="@yield('head_description', getcong('site_description'))"/>
+
+    <meta property="og:image" content="@yield('head_image', url('/upload/logo.png'))" />
+    <meta property="og:url" content="@yield('head_url', url('/'))" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+		
 	<link rel="stylesheet" href="/mobile/css/materialize.css">
 	<link rel="stylesheet" href="/mobile/font-awesome/css/font-awesome.min.css">
 	<link rel="stylesheet" href="/mobile/css/normalize.css">
