0632e05d3cdf4f08b6157b8b034792cf171d76e4
Pavel496 <butusovpn@mail.ru>
2018-08-30 16:54:41 +0300

    Есть

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -30,7 +30,7 @@
 
       <div class="property-box col-xs-12 col-sm-6 col-md-4">
         <div class="inner-box">
-          <a href="{{ url('properties/'.$property->property_slug.'/'.Crypt::encryptString($property->id)) }}" class="img-container">
+          <a href="{{ url('properties/'.$property->property_type.'/'.Crypt::encryptString($property->id)) }}" class="img-container">
 
             @if($property->featured_property==1)<span class="tag-label hot-offer">Избранное</span>@endif
             <span class="my1-label featured">{{ $property->direction_to->name }}</span>
