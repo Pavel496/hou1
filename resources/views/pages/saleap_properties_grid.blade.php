a6e5045d4b3bcd3372788493bcb8c7f2fbc62afa
Pavel496 <butusovpn@mail.ru>
2018-09-01 11:44:59 +0300

    property_type. Есть

--- resources/views/pages/saleap_properties_grid.blade.php
+++ resources/views/pages/saleap_properties_grid.blade.php
@@ -36,7 +36,7 @@
            @foreach($chunked_property as $property)
       <div class="property-box col-xs-12 col-sm-6 col-md-4">
         <div class="inner-box">
-          <a href="{{ url('properties/'.$property->property_slug.'/'.Crypt::encryptString($property->id)) }}" class="img-container">
+          <a href="{{ url('properties/'.$property->property_type.'/'.Crypt::encryptString($property->id)) }}" class="img-container">
 
             @if($property->featured_property==1)<span class="tag-label hot-offer">Избранное</span>@endif
             <span class="my1-label featured">{{ $property->direction_to->name }}</span>
@@ -57,27 +57,29 @@
             {{-- <span class="price">{{$property->currency.' '.number_format($property->price)}}</span> --}}
           </a>
           <div class="bottom-sec">
-            <a href="{{ url('properties/'.$property->property_slug.'/'.Crypt::encryptString($property->id)) }}" class="title">{{ str_limit($property->property_name,35) }}</a>
+            <a href="{{ url('properties/'.$property->property_type.'/'.Crypt::encryptString($property->id)) }}" class="title">{{ str_limit($property->property_name,35) }}</a>
             <div class="location">{{ str_limit($property->address,40) }}</div>
             <div class="desc">
               {!! str_limit($property->description,100) !!}
             </div>
+
             <div class="extra-info clearfix">
               <div class="area col-xs-4">
-                <div class="value">{{$property->range}}</div>
-                км
+                <div class="value">{{$property->bedrooms}}</div>
+                комн
               </div>
               <div class="bedroom col-xs-4">
-                <div class="value">{{$property->land_area}}</div>
-                сот
+                <div class="value">{{$property->bathrooms}}</div>
+                с/у
               </div>
               <div class="bathroom col-xs-4">
                 <div class="value">{{$property->build_area}}</div>
                 м2
               </div>
             </div>
+
           </div>
-          <a href="{{ url('properties/'.$property->property_slug.'/'.Crypt::encryptString($property->id)) }}" class="btn more-link">Подробнее</a>
+          <a href="{{ url('properties/'.$property->property_type.'/'.Crypt::encryptString($property->id)) }}" class="btn more-link">Подробнее</a>
         </div>
       </div>
     @endforeach
