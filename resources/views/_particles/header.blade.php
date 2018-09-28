f777ed08c63669e43556754972319d297a2dac00
Pavel496 <butusovpn@mail.ru>
2018-09-28 19:57:22 +0300

    Меню. Есть

--- resources/views/_particles/header.blade.php
+++ resources/views/_particles/header.blade.php
@@ -44,13 +44,13 @@
                 <!-- Main Menu -->
                 <nav id="main-menu" class="hidden-xs hidden-sm">
                     <ul class="main-menu list-inline">
-                        <li><a href="{{ URL::to('/') }}" class="{{classActivePathPublic('')}}">Все объекты</a></li>
+                        <li><a style="font-size:22px" href="{{ URL::to('/') }}" class="{{classActivePathPublic('')}}">Все объекты</a></li>
                         <!-- Menu items ( You can change the link and its text ) -->
 
                         {{-- <li><a href="{{ URL::to('properties/') }}" class="{{classActivePathPublic('properties')}}">All Properties</a></li>
                         <li><a href="{{ URL::to('featured/') }}" class="{{classActivePathPublic('featured')}}">Featured</a></li> --}}
                         <li class="dropdown">
-                          <a class="dropdown-toggle {{classActivePathPublic('house')}}" data-toggle="dropdown" role="button" aria-haspopup="false" aria-expanded="true">Дома</a>
+                          <a style="font-size:22px" class="dropdown-toggle {{classActivePathPublic('house')}}" data-toggle="dropdown" role="button" aria-haspopup="false" aria-expanded="true">Дома</a>
                           <ul class="dropdown-menu">
                             <li><a href="{{ URL::to('house/sale') }}" class="{{classActivePathPublic('house/sale')}}">Продажа домов</a></li>
                             <li><a href="{{ URL::to('house/rent') }}" class="{{classActivePathPublic('house/rent')}}">Аренда домов</a></li>
@@ -59,7 +59,7 @@
                         </li>
 
                         <li class="dropdown">
-                          <a class="dropdown-toggle {{classActivePathPublic('apartment')}}" data-toggle="dropdown" role="button" aria-haspopup="false" aria-expanded="true">Квартиры</a>
+                          <a style="font-size:22px" class="dropdown-toggle {{classActivePathPublic('apartment')}}" data-toggle="dropdown" role="button" aria-haspopup="false" aria-expanded="true">Квартиры</a>
                           <ul class="dropdown-menu">
                             <li><a href="{{ URL::to('apartment/sale') }}" class="{{classActivePathPublic('apartment/sale')}}">Продажа квартир</a></li>
                             <li><a href="{{ URL::to('apartment/rent') }}" class="{{classActivePathPublic('apartment/rent')}}">Аренда квартир</a></li>
@@ -69,10 +69,10 @@
 
                         {{-- <li><a href="{{ URL::to('agents/') }}" class="{{classActivePathPublic('agents')}}">Агенты</a> --}}
                         {{-- <li><a href="{{ URL::to('testimonials/') }}" class="{{classActivePathPublic('testimonials')}}">Testimonials</a></li> --}}
-                        <li><a href="{{ URL::to('advices/') }}" class="{{classActivePathPublic('advices')}}">Советы эксперта</a>
-                        <li><a href="{{ URL::to('about-us/') }}" class="{{classActivePathPublic('about-us')}}">О нас</a></li>
-                        <li><a href="{{ URL::to('design/') }}" class="{{classActivePathPublic('design')}}">Строительство и дизайн</a></li>
-                        <li><a href="{{ URL::to('contact-us/') }}" class="{{classActivePathPublic('contact-us')}}">Контакты</a></li>
+                        <li><a style="font-size:15px" href="{{ URL::to('advices/') }}" class="{{classActivePathPublic('advices')}}">Советы эксперта</a>
+                        <li><a style="font-size:15px" href="{{ URL::to('about-us/') }}" class="{{classActivePathPublic('about-us')}}">О нас</a></li>
+                        <li><a style="font-size:15px" href="{{ URL::to('design/') }}" class="{{classActivePathPublic('design')}}">Строительство и дизайн</a></li>
+                        <li><a style="font-size:15px" href="{{ URL::to('contact-us/') }}" class="{{classActivePathPublic('contact-us')}}">Контакты</a></li>
 {{-- <li class="dropdown">
   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
   <ul class="dropdown-menu">
